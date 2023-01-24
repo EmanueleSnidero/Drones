33079f340c17c715970a4fdfa3d2528b0e16fae6
EmanueleSnidero <emanuele.snidero@stud.itsaltoadriatico.it>
2023-01-24 18:27:06 +0100

    Drone class and drones list

---
+++ Client/Fleet/Fleet/Drone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fleet
+{
+    internal class Drone
+    {
+        private Random valueGenerator;
+        public int id { get; set; }
+        public bool state { get; set; }
+        public Drone(int id)
+        {
+            valueGenerator = new Random();
+            this.id = id;
+            this.state = true;
+        }
+        public int getSpeed() 
+        { 
+            return valueGenerator.Next(0, 60);
+        }
+        public Position getPosition()
+        {
+            return new Position(valueGenerator.Next(0, 100), valueGenerator.Next(0, 100), valueGenerator.Next(0, 100));
+        }
+        public int getBattery()
+        {
+            return valueGenerator.Next(0, 100);
+        }
+        public void changeState(bool command)
+        {
+            this.state = command;
+        }
+        public DateTime getTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
